c750af8aaf55e623d54ecc5cd6aae5577a353a04
A. Ichou <aichou@innovatieweb.nl>
2025-12-10 20:54:56 +0100

    install drizzle and create config

---
+++ drizzle.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  out: "./drizzle",
+  dialect: "postgresql",
+  schema: "./src/lib/server/db/schema",
+
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+
+  extensionsFilters: ["postgis"],
+  schemaFilter: "public",
+  tablesFilter: "*",
+
+  introspect: {
+    casing: "camel",
+  },
+
+  migrations: {
+    prefix: "timestamp",
+    table: "__drizzle_migrations__",
+    schema: "public",
+  },
+
+  entities: {
+    roles: {
+      provider: '',
+      exclude: [],
+      include: []
+    }
+  },
+
+  breakpoints: true,
+  strict: true,
+  verbose: true,
+});
+
