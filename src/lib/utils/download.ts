fcdc2dd086890abf2f3a0501ec450d549c111d64
A. Ichou <aichou@innovatieweb.nl>
2025-12-13 11:00:18 +0100

    add axios instance

--- src/lib/utils/download.ts
+++ src/lib/utils/download.ts
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import api from './axios';
 
 export async function handleDownloadAttachment(ticketId: number, fileName: string) {
-  const response = await axios.get(`/dashboard/tickets/${ticketId}/${fileName}`, {
+  const response = await api.get(`/dashboard/tickets/${ticketId}/${fileName}`, {
     responseType: 'blob'
   });
 
